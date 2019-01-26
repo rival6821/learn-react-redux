8c1fb0e761b6bde72d04186407422cc346b353ea
ilhoon lee <rival6821@naver.com>
2019-01-26 22:21:38 +0900

    sample upload

---
+++ src/containers/PaletteContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PaletteContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PaletteContainer;
